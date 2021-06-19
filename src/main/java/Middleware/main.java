1a29ff7358a48ca2505df409f0fcbb5010c6814d
HuamanAngel <oxipusioxx12@gmail.com>
2021-06-18 20:45:17 -0500

    Base

---
+++ src/main/java/Middleware/main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Middleware;
+
+/**
+ *
+ * @author oxipusio
+ */
+public class main {
+    public static void main(String[] args){
+        System.out.println("vaca");
+    }
+    
+}
